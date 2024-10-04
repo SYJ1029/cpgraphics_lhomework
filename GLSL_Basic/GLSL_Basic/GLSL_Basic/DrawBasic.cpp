3d2c0e2d4fb7585186a346355e466fad83d795a3
LAPTOP-KAKG2S7N\abg09 <abg0986@gmail.com>
2024-10-04 14:34:21 +0900

    기초 정리가 어느정도 되었고, 7번 문제를 풀 준비

---
+++ GLSL_Basic/GLSL_Basic/GLSL_Basic/DrawBasic.cpp
@@ -0,0 +1,72 @@
+#include "GetShader.h"
+
+
+GLvoid Keyboard(unsigned char key, int x, int y);
+MyCol mycolor = { 1.0f, 1.0f, 1.0f, 1.0f };
+
+
+void main(int argc, char** argv) { //--- 윈도우 출력하고 콜백함수 설정 { //--- 윈도우 생성하기
+	glutInit(&argc, argv); // glut 초기화
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA); // 디스플레이 모드 설정
+	glutInitWindowPosition(0, 0); // 윈도우의 위치 지정
+	glutInitWindowSize(800, 600); // 윈도우의 크기 지정
+	glutCreateWindow("Example1"); // 윈도우 생성
+	//--- GLEW 초기화하기
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) // glew 초기화
+	{
+		std::cerr << "Unable to initialize GLEW" << std::endl
+			;
+		exit(EXIT_FAILURE);
+	}
+	else
+		cout << "GLEW Initialized\n";
+
+
+	make_vertexShaders();
+	make_fragmentShaders();
+	make_shaderProgram();
+	InitBuffer();
+
+	glutDisplayFunc(drawScene); // 출력 함수의 지정
+	glutReshapeFunc(Reshape); // 다시 그리기 함수 지정
+	glutKeyboardFunc(Keyboard);
+	glutMainLoop(); // 이벤트 처리 시작
+}
+
+
+
+GLvoid drawScene() {
+	glClearColor(mycolor.red, mycolor.green, mycolor.blue, mycolor.alpha);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // 설정된 색으로 전체를 칠하기
+	// 그리기 부분 구현: 그리기 관련 부분이 여기에 포함된다
+
+	glUseProgram(shaderProgramID);
+	glBindVertexArray(vao);
+	UpdateBuffer();
+
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+
+	glutSwapBuffers(); // 화면에 출력하기
+
+
+}
+
+GLvoid Reshape(int w, int h) { //--- 콜백 함수: 다시 그리기 콜백 함수 {
+	glViewport(0, 0, w, h);
+}
+
+GLvoid Keyboard(unsigned char key, int x, int y) {
+
+	switch (key) {
+	case 'd':
+		triShape[2][0] = triShape[2][0] + 0.05;
+		InitBuffer();
+		break;
+	default:
+		break;
+	}
+
+	glutPostRedisplay();
+}
