8cbe1a41b96badb005e55c5f5cf88bae0df253a5
LAPTOP-KAKG2S7N\abg09 <abg0986@gmail.com>
2024-10-27 17:41:00 +0900

    한 좌표를 중심으로, 지정된 축을 따라 radian 만큼 회전시키는 함수를 구현하자.
    어떤 객체를 사용하던 내부 형태의 변화 없이 모든 객체던 구조체던 동일하게 사용하는 함수니가, 클래스의 멤버 함수가 아니라 일반 함수로서 정의하자
    
    성공!

--- GLSL_Basic/GLM_Transform/GLM_Transform/GLM3D_NO12.cpp
+++ GLSL_Basic/GLM_Transform/GLM_Transform/GLM3D_NO12.cpp
@@ -3,40 +3,81 @@
 #define MAX_INDEX 12
 #define MAX_INDEX13 2
 
+#define ID_CUBE 0
+#define ID_TET 1
+#define ID_PYR 2
+
 GLvoid Mouse(int button, int state, int x, int y);
 GLvoid Keyboard(unsigned char key, int x, int y);
-GLvoid MyTimer(int index);
+GLvoid MyCcw(int value);
+GLvoid MyCw(int value);
+GLvoid specialKeyboard(int key, int x, int y);
+
 
 
 MyCol mycolor = { 1.0f, 1.0f, 1.0f, 1.0f };
 GLPos Screensize = { 800, 800, 0 };
 GL_Cube* cube = new GL_Cube;
 GL_Tetrahedron* tri = new GL_Tetrahedron;
+GL_Pyramid* pyr = new GL_Pyramid;
+
+bool depthed = true;
+bool gospin = false;
+bool direct = true;
+
+void DepthCheck() {
+	if (depthed)
+		glEnable(GL_DEPTH_TEST);
+	else
+		glDisable(GL_DEPTH_TEST);
+}
 
 void Setindex() {
 	int* p1 = cube->AddIndexList();
 	int* p2 = tri->AddIndexList();
+	int* p3 = pyr->AddIndexList();
 
 	cout << sizeof((p1));
 
 	int present_bit = index_count;
+	int cnt = 0;
+	int begin = cnt;
+
 
 	for (index_count; index_count < 36; index_count++) {
 		index[index_count] = p1[index_count];
+
+		cnt++;
 	}
+
+	//cnt += 36;
 
 	present_bit = index_count;
 
+	begin = cnt;
 	for (index_count; index_count < present_bit + 12; index_count++) {
-		index[index_count] = 8 + p2[index_count - 36];
+		index[index_count] = 8 + p2[index_count - begin];
+
+		cnt++;
 	}
 
+	//cnt += 12;
+
+	present_bit = index_count;
+	begin = cnt;
+
+	for (index_count; index_count < present_bit + 18; index_count++) {
+		index[index_count] = 12 + p3[index_count - begin];
+	}
+
+	cnt += 15;
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index), index, GL_DYNAMIC_DRAW);
 
 	free(p1);
 	free(p2);
+	free(p3);
 }
 
 MyObjCol SetRandObjCol() {
@@ -46,13 +87,29 @@ MyObjCol SetRandObjCol() {
 }
 
 GLvoid SetBuffer() {
+	MyObjCol mycol[8];
+	MyObjCol mycol2[4];
+	MyObjCol mycol3[5];
 
+	for (int i = 0; i < 8; i++) {
+		mycol[i] = SetRandObjCol();
+	}
+	for (int i = 0; i < 4; i++) {
+		mycol2[i] = SetRandObjCol();
+	}
+	for (int i = 0; i < 5; i++) {
+		mycol3[i] = SetRandObjCol();
+	}
+	cube->Setcol(mycol);
+	tri->Setcol(mycol2);
+	pyr->Setcol(mycol3);
 
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
 	//glBufferData(GL_ARRAY_BUFFER, 18 * sizeof(GLfloat), NULL, GL_DYNAMIC_DRAW);
 	glBufferData(GL_ARRAY_BUFFER, (MAX_INDEX * 10000) * sizeof(GLfloat), NULL, GL_DYNAMIC_DRAW);
 	cube->SetPos();
 	tri->SetPos();
+	pyr->SetPos();
 
 	//float* counter = new FLOAT();
 	int* counter = new INT();
@@ -72,6 +129,13 @@ GLvoid SetBuffer() {
 	for (int i = 0; i < 4; i++) {
 		glBufferSubData(GL_ARRAY_BUFFER, (*counter),
 			3 * sizeof(GLfloat), tri->pos[i]);
+
+		(*counter) += 3 * sizeof(GLfloat);
+	}
+
+	for (int i = 0; i < 5; i++) {
+		glBufferSubData(GL_ARRAY_BUFFER, (*counter),
+			3 * sizeof(GLfloat), pyr->pos[i]);
 
 		(*counter) += 3 * sizeof(GLfloat);
 	}
@@ -104,6 +168,13 @@ GLvoid SetBuffer() {
 	for (int i = 0; i < 4; i++) {
 		glBufferSubData(GL_ARRAY_BUFFER, (*counter),
 			3 * sizeof(GLfloat), tri->col[i]);
+
+		(*counter) += 3 * sizeof(GLfloat);
+	}
+
+	for (int i = 0; i < 5; i++) {
+		glBufferSubData(GL_ARRAY_BUFFER, (*counter),
+			3 * sizeof(GLfloat), pyr->col[i]);
 
 		(*counter) += 3 * sizeof(GLfloat);
 	}
@@ -141,26 +212,20 @@ void main(int argc, char** argv) { //--- 윈도우 출력하고 콜백함수 설정 { //--- �
 	InitBuffer();
 
 
-	MyObjCol mycol[8];
-	MyObjCol mycol2[4];
-	for (int i = 0; i < 8; i++) {
-		mycol[i] = SetRandObjCol();
-	}
-	for (int i = 0; i < 4; i++) {
-		mycol2[i] = SetRandObjCol();
-	}
-	cube->Setcol(mycol);
-	tri->Setcol(mycol2);
 
 	glBindVertexArray(vao);
 	SetBuffer();
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
+	glEnable(GL_DEPTH_TEST);
+
 
 	glutDisplayFunc(drawScene); // 출력 함수의 지정
 	glutReshapeFunc(Reshape); // 다시 그리기 함수 지정
 	glutKeyboardFunc(Keyboard);
+	glutSpecialFunc(specialKeyboard);
 	glutMouseFunc(Mouse);
+
 
 
 	glutMainLoop(); // 이벤트 처리 시작
@@ -177,13 +242,14 @@ void drawCube(float* counter, int index) {
 }
 
 float radian = 10.0f;
-int mycnt = 1;
+int mycnt = 0;
+glm::vec3 allAxis = glm::vec3(0.0f, 0.0f, 0.0f);
 
 void drawScene()
 {
-	void* drawStage;
+	Diagram* drawStage = nullptr;
 
-	
+	drawStage = cube;
 
 
 	glClearColor(mycolor.red, mycolor.green, mycolor.blue, mycolor.alpha);
@@ -192,21 +258,26 @@ void drawScene()
 	//gluLookAt(0.0, 0.0, 100.0f, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 
 	glUseProgram(shaderProgramID);
-	glm::mat4 model = glm::mat4(1.0f);
-	glm::mat4 rm = model;
-	glm::mat4 rm2 = model;
+	glm::mat4 basemat = glm::mat4(1.0f);
+	glm::mat4 rm = basemat;
+	glm::mat4 rm2 = basemat;
+	glm::mat4 model = basemat;
+	//glm::mat4 rm3 = glm::mat4(1.0f);
 
 	/*model = glm::translate(model, glm::vec3(0.1f, 0.5f, 0.0f));*/
+	
 
-
-
-
-	rm = glm::rotate(model, glm::radians(10.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-	rm2 = glm::rotate(model, glm::radians(radian), glm::vec3(0.0f, 1.0f, 0.0f));
+	rm = glm::rotate(basemat, glm::radians(30.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	rm2 = glm::rotate(basemat, glm::radians(30.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 
 	model = rm2 * rm;
 
-	unsigned int modelLocation = glGetUniformLocation(shaderProgramID, "modelTransform");
+	model *= GetMove(cube->center);
+	model *= GetSpin(cube->center, radian, cube->axis);
+
+
+
+	int modelLocation = glGetUniformLocation(shaderProgramID, "modelTransform");
 	glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
 
 
@@ -217,9 +288,6 @@ void drawScene()
 
 	int counter = 0;
 
-	//(*counter) += 30 * sizeof(float);
-	/*glDrawElements(GL_QUADS, 3, GL_UNSIGNED_INT, (void*)counter);*/
-	//glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, (void*)counter);
 
 	for (int i = 0; i < 6; i++) {
 		if (cube->maked[i]) {
@@ -237,6 +305,19 @@ void drawScene()
 
 	}
 
+	model = basemat;
+
+	rm = glm::rotate(basemat, glm::radians(30.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	rm2 = glm::rotate(basemat, glm::radians(30.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+	model = rm2 * rm;
+
+	model *= GetMove(tri->center);
+	model *= GetSpin(tri->center, radian, tri->axis);
+
+	glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
+
+
 	for (int i = 0; i < 4; i++) {
 		if (tri->maked[i]) {
 			glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, (void*)counter);
@@ -247,10 +328,38 @@ void drawScene()
 		}
 	}
 
+	model = basemat;
+
+	rm = glm::rotate(basemat, glm::radians(30.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	rm2 = glm::rotate(basemat, glm::radians(30.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+	model = rm2 * rm;
+
+	model *= GetMove(pyr->center);
+	model *= GetSpin(pyr->center, radian, pyr->axis);
+
+	glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
+
+
+	for (int i = 0; i < 5; i++) {
+		if (pyr->maked[i]) {
+			glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, (void*)counter);
+			counter += 3 * sizeof(GLfloat);
+
+			if (i >= 4) {
+				glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, (void*)counter);
+				counter += 3 * sizeof(GLfloat);
+			}
+		}
+		else {
+			counter += 3 * sizeof(GLfloat);
+			if(i >= 4)
+				counter += 3 * sizeof(GLfloat);
+		}
+	}
 
 
 
-	glEnable(GL_DEPTH_TEST);
 	glutSwapBuffers();
 
 }
@@ -265,6 +374,10 @@ GLvoid Mouse(int button, int state, int x, int y) {
 
 	glutPostRedisplay();
 }
+
+
+
+
 GLvoid Keyboard(unsigned char key, int x, int y) {
 
 	int token = (int)key - (int)'1';
@@ -272,75 +385,178 @@ GLvoid Keyboard(unsigned char key, int x, int y) {
 	randnum[0] = -1, randnum[1] = -1;
 
 	switch (key) {
-	case '1': case '2': case '3': case '4': case '5': case '6':
-		cube->maked[token] = !(cube->maked[token]);
-		break;
-
-	case '7':case '8': case '9': case '0':
-		token -= 6;
-
-		if (token < 0)
-			token = 3;
-
-		tri->maked[token] = !(tri->maked[token]);
-		break;
 	case 'c':
 		for (int i = 0; i < 6; i++) {
 			if (i < 4) {
 				tri->maked[i] = false;
 			}
 
-
-			cube->maked[i] = false;
-		}
-
-		for (int i = 0; i < 2; i++) {
-			randnum[i] = (float)((float)rand() / RAND_MAX) * 6;
-
-			if (i > 0 && randnum[i] == randnum[i - 1]) {
-				i--;
+			if (i < 5) {
+				pyr->maked[i] = false;
 			}
+			
+			cube->maked[i] = true;
 		}
-
-
-
-
-		cube->maked[randnum[0]] = true;
-		cube->maked[randnum[1]] = true;
-
 
 
 		break;
 	case 't':
 		for (int i = 0; i < 6; i++) {
 			if (i < 4) {
-				tri->maked[i] = false;
+				tri->maked[i] = true;
 			}
 
+			if (i < 5) {
+				pyr->maked[i] = false;
+			}
 
 			cube->maked[i] = false;
 		}
 
-		for (int i = 0; i < 2; i++) {
-			randnum[i] = (float)((float)rand() / RAND_MAX) * 4;
+		break;
 
-			if (i > 0 && randnum[i] == randnum[i - 1]) {
-				i--;
+	case 'p':
+		for (int i = 0; i < 6; i++) {
+			if (i < 4) {
+				tri->maked[i] = false;
 			}
+
+			if (i < 5) {
+				pyr->maked[i] = true;
+			}
+
+			cube->maked[i] = false;
+		}
+		break;
+	case 'h':
+		depthed = !(depthed);
+		DepthCheck();
+		break;
+
+	case 'w': case 'W':
+		if ((int)key - (int)'a' < 0) {
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		}
+		else {
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
 		}
 
-		tri->maked[randnum[0]] = true;
-		tri->maked[randnum[1]] = true;
+		break;
+
+	case 'x': case 'X':
+		gospin = true;
+
+		if ((int)key - (int)'a' < 0) {
+			direct = true;
+
+			cube->axis = glm::vec3(1.0f, 0.0f, 0.0f);
+			tri->axis = glm::vec3(1.0f, 0.0f, 0.0f);
+			pyr->axis = glm::vec3(1.0f, 0.0f, 0.0f);
+			glutTimerFunc(10, MyCcw, 0);
+		}
+		else {
+			direct = false;
+
+			cube->axis = glm::vec3(1.0f, 0.0f, 0.0f);
+			tri->axis = glm::vec3(1.0f, 0.0f, 0.0f);
+			pyr->axis = glm::vec3(1.0f, 0.0f, 0.0f);
+			glutTimerFunc(10, MyCw, 0);
+		}
 
 		break;
+	case 'y': case 'Y':
+		gospin = true;
+
+		if ((int)key - (int)'a' < 0) {
+			direct = true;
+
+			cube->axis = glm::vec3(0.0f, 1.0f, 0.0f);
+			tri->axis = glm::vec3(0.0f, 1.0f, 0.0f);
+			pyr->axis = glm::vec3(0.0f, 1.0f, 0.0f);
+			glutTimerFunc(10, MyCcw, 0);
+		}
+		else {
+			direct = false;
+
+			cube->axis = glm::vec3(0.0f, 1.0f, 0.0f);
+			tri->axis = glm::vec3(0.0f, 1.0f, 0.0f);
+			pyr->axis = glm::vec3(0.0f, 1.0f, 0.0f);
+			glutTimerFunc(10, MyCw, 0);
+
+		}
+
+		break;
+	case 's':
+		gospin = false;
+		cube->center = { 0 };
+		tri->center = { 0 };
+		pyr->center = { 0 };
+		break;
+
+	
+
+	case 'q':
+
+		delete cube;
+		delete tri;
+		delete pyr;
+		glutLeaveMainLoop();
+		break;
+
 	default:
 		break;
 	}
 
 	glutPostRedisplay();
 }
-GLvoid MyTimer(int index) {
+
+GLvoid specialKeyboard(int key, int x, int y) {
+	switch (key) {
+	case GLUT_KEY_LEFT:
+		cube->center.x -= 0.02f;
+		tri->center.x -= 0.02f;
+		pyr->center.x -= 0.02f;
+		break;
+	case GLUT_KEY_RIGHT:
+		cube->center.x += 0.02f;
+		tri->center.x += 0.02f;
+		pyr->center.x += 0.02f;
+		break;
+	case GLUT_KEY_UP:
+		cube->center.y += 0.02f;
+		tri->center.y += 0.02f;
+		pyr->center.y += 0.02f;
+		break;
+	case GLUT_KEY_DOWN:
+		cube->center.y -= 0.02f;
+		tri->center.y -= 0.02f;
+		pyr->center.y -= 0.02f;
+		break;
+	}
+
+	glutPostRedisplay();
+}
 
 
+
+GLvoid MyCcw(int value) {
+
+	radian += 5;
+
+
+	if (gospin && direct) {
+		glutTimerFunc(10, MyCcw, value);
+	}
+	glutPostRedisplay();
+}
+GLvoid MyCw(int value) {
+
+	radian -= 5;
+
+
+	if (gospin && direct==false) {
+		glutTimerFunc(10, MyCw, value);
+	}
 	glutPostRedisplay();
 }
--- GLSL_Basic/GLM_Transform/GLM_Transform/Head/3DDiagram.h
+++ GLSL_Basic/GLM_Transform/GLM_Transform/Head/3DDiagram.h
@@ -7,6 +7,39 @@
 int index_count = 0;
 int index_array_count = 0;
 
+glm::mat4 GetMove(GLPos Delta) {
+	glm::mat4 basemat = glm::mat4(1.0f);
+	glm::vec3 trv = glm::vec3(Delta.x, Delta.y, Delta.z);
+	glm::mat4 trm = basemat;
+	glm::mat4 model = basemat;
+
+	trm = glm::translate(basemat, trv);
+
+
+	model *= trm;
+
+	return model;
+}
+
+glm::mat4 GetSpin(GLPos Center, GLfloat radian, glm::vec3 axis) {
+	glm::mat4 basemat = glm::mat4(1.0f);
+	glm::mat4 trm = basemat;
+	glm::mat4 rm = basemat;
+	glm::mat4 rm2 = basemat;
+	glm::mat4 model = basemat;
+
+	glm::vec3 trv = glm::vec3(Center.x, Center.y, Center.z);
+
+	trm = glm::translate(basemat, trv * -1.0f);
+	rm = glm::rotate(basemat, glm::radians(radian), axis);
+	glm::mat4 trm2 = glm::translate(basemat, trv);
+
+	model = trm2 * rm * trm;
+
+	return model;
+}
+
+
 
 class Diagram {
 
@@ -14,30 +47,70 @@ class Diagram {
 
 public:
 	int postype;
-	GLfloat center[3];
-	GLfloat initial_center[3];
-
+	glm::vec3 axis;
+	GLPos center;
+	float radian;
+	int radcnt;
+	GLPos delta;
 
 	Diagram() {
-		for (int i = 0; i < 3; i++) {
-			center[i] = 0;
-			initial_center[i] = 0;
-		}
 
 		postype = POINT;
+
+		radian = 0;
+		center = { 0, 0, 0 };
+		axis = glm::vec3( 0.0f, 1.0f, 0.0f );
+		delta = { 0, 0, 0 };
+
+		radcnt = 1;
 	}
 
-	void Setcol() {
 
-	}
-	
-
-	void SetPos() {
-
+	void Revert() {
+		delta = { 0, 0, 0 };
 	}
 
-	int* AddIndexList() {
+	void Clear() {
+		Revert();
+		radian = 0;
+		axis = { 0, 0, 0 };
+		
+		radcnt = 1;
 	}
+
+	virtual void Setcol(MyObjCol col[]) = 0;
+
+	virtual void SetPos() = 0;
+
+	virtual int* AddIndexList() = 0;
+
+	virtual ~Diagram() {
+		cout << "Diagram is deleted" << endl << endl;
+	}
+
+
+	void SetSpin(bool ccw, glm::vec3 axis) {
+
+		this->axis = axis;
+
+		if (ccw)
+			radcnt = 1;
+		else
+			radcnt = -1;
+	}
+
+	void Spin() {
+		radian += radcnt;
+	}
+
+	void Move(GLPos delta) {
+		this->delta.x += delta.x;
+		this->delta.y += delta.y;
+		this->delta.z += delta.z;
+	}
+
+
+
 };
 
 
@@ -217,7 +290,7 @@ public:
 
 
 
-class Pyramid : public Diagram{
+class GL_Pyramid : public Diagram{
 
 
 public:
@@ -226,7 +299,7 @@ public:
 	bool maked[5];
 	
 
-	Pyramid(): Diagram(){
+	GL_Pyramid() : Diagram() {
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 3; j++) {
 				col[i][j] = 0;
@@ -238,5 +311,55 @@ public:
 		}
 	}
 
+	void Setcol(MyObjCol col[5]) {
+		for (int i = 0; i < 5; i++) {
+			this->col[i][0] = col[i].R;
+			this->col[i][1] = col[i].G;
+			this->col[i][2] = col[i].B;
+
+		}
+	}
+
+
+	void SetPos() {
+		pos[0][0] = 0.0f, pos[0][1] = 0.5f, pos[0][2] = 0.0f;
+		pos[1][0] = -0.5f, pos[1][1] = -0.5f, pos[1][2] = -0.5f;
+		pos[2][0] = 0.5f, pos[2][1] = -0.5f, pos[2][2] = -0.5f;
+		pos[3][0] = 0.5f, pos[3][1] = -0.5f, pos[3][2] = 0.5f;
+		pos[4][0] = -0.5f, pos[4][1] = -0.5f, pos[4][2] = 0.5f;
+
+	}
+
+	int* AddIndexList() {
+		static int* p = (int*)malloc(3 * 6 * sizeof(int));
+
+		cout << sizeof(*p) << endl << endl;
+
+		p[0] = 0;
+		p[1] = 1;
+		p[2] = 2;
+
+		p[3] = 0;
+		p[4] = 2;
+		p[5] = 3;
+
+		p[6] = 0;
+		p[7] = 3;
+		p[8] = 4;
+
+		p[9] = 0;
+		p[10] = 4;
+		p[11] = 1;
+
+		p[12] = 1;
+		p[13] = 2; 
+		p[14] = 3;
+
+		p[15] = 1;
+		p[16] = 3;
+		p[17] = 4;
+
+		return p;
+	}
 
 };
