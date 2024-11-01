1671a566a161145cb8f73210dd2d337f490c311e
LAPTOP-KAKG2S7N\abg09 <abg0986@gmail.com>
2024-11-01 15:29:54 +0900

    표준 헤더파일을 추가하는 부분을 BaseHeader로 옮겨놓고 큐의 형식을 지정한 헤더파일을 생성했다,

--- GLSL_Basic/GLM_Transform/GLM_Transform/Head/ColorStructs.h
+++ GLSL_Basic/GLM_Transform/GLM_Transform/Head/ColorStructs.h
@@ -1,14 +1,6 @@
 #pragma once
 
-
-#include <iostream>
-#include <gl/glew.h> 
-#include <gl/freeglut.h>
-#include <gl/freeglut_ext.h>
-#include <glm/glm.hpp>
-#include <glm/ext.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-using namespace std;
+#include "BaseHeader.h"
 
 typedef struct Mycolor {
 	GLclampf red;
